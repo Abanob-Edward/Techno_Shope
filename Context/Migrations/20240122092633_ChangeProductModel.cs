f815241f9c79f3a908ea6767c72275005d6196d4
abanob-code <72308337+abanob-code@users.noreply.github.com>
2024-01-22 11:32:38 +0200

    new Migrate to change prodeuct model

---
+++ Context/Migrations/20240122092633_ChangeProductModel.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeProductModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CatID",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_AdminID",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Cat_id",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CatID",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AdminID",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CatID",
+                table: "Products",
+                column: "CatID",
+                principalTable: "Categories",
+                principalColumn: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_AdminID",
+                table: "Products",
+                column: "AdminID",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CatID",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_AdminID",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CatID",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AdminID",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Cat_id",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CatID",
+                table: "Products",
+                column: "CatID",
+                principalTable: "Categories",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_AdminID",
+                table: "Products",
+                column: "AdminID",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
